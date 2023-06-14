7a972e58b6354bdb850785e4cb605ad168a755f1
emin9 <emsambiev@gmail.com>
2023-06-14 20:17:01 +0300

    first commit
    first commit

---
+++ check4/src/base.jsx
@@ -0,0 +1,13 @@
+import logo from './logo192.png'
+import Detective from './detective'
+
+function Base(){
+    return(
+        <div>
+          Шерлок - <Detective/>
+             
+        </div>
+    )
+}
+
+export default Base
---
+++ check4/src/detective.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+function Detective(){
+  const [sherlock,setSherlock] = useState('психопат')
+    return(
+        sherlock
+    )
+}
+export default Detective
